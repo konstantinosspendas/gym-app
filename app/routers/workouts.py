77183f6533c52e9cb578976ec885e16cf8858f70
konstantinosspendas <p20spen@ionio.gr>
2026-09-20 16:22:08 +0300

    delete/update gia workouts

--- app/routers/workouts.py
+++ app/routers/workouts.py
@@ -49,3 +49,30 @@ def create_workout(
     db.refresh(new_workout)
 
     return new_workout
+
+
+@router.put("/{workout_id}")
+def update_workout(
+    workout_id: int,
+    workout_data: schemas.WorkoutCreate,
+    db: Session = Depends(get_db)
+):
+    workout = db.query(models.Workout).filter(
+        models.Workout.id == workout_id
+    ).first()
+
+    if workout is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Workout not found"
+        )
+
+    workout.title = workout_data.title
+    workout.date = workout_data.date
+    workout.duration = workout_data.duration
+    workout.notes = workout_data.notes
+
+    db.commit()
+    db.refresh(workout)
+
+    return workout
