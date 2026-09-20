8528c5d0007e672023fd6a76b47bc50e30286c28
konstantinosspendas <p20spen@ionio.gr>
2026-09-20 12:47:33 +0300

    workouts router fixes

--- app/routers/workouts.py
+++ app/routers/workouts.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import get_db
-from app import models
+from app import models, schemas
 
 
 router = APIRouter(
@@ -15,3 +15,37 @@ router = APIRouter(
 def get_workouts(db: Session = Depends(get_db)):
     workouts = db.query(models.Workout).all()
     return workouts
+
+
+@router.get("/{workout_id}")
+def get_workout(workout_id: int, db: Session = Depends(get_db)):
+    workout = db.query(models.Workout).filter(
+        models.Workout.id == workout_id
+    ).first()
+
+    if workout is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Workout not found"
+        )
+
+    return workout
+
+
+@router.post("/")
+def create_workout(
+    workout: schemas.WorkoutCreate,
+    db: Session = Depends(get_db)
+):
+    new_workout = models.Workout(
+        title=workout.title,
+        date=workout.date,
+        duration=workout.duration,
+        notes=workout.notes
+    )
+
+    db.add(new_workout)
+    db.commit()
+    db.refresh(new_workout)
+
+    return new_workout
